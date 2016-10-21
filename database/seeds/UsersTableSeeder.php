7f791ba5ec663c3865a2f043b402afa98a50198b
emillion <echandra@terhubung.com>
2016-10-22 04:10:15 +0700

    add user seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +12,28 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->delete();
+        DB::table('users')->insert([
+            [
+                "id" => "1",
+                "name" => "Bagus Setiawan",
+                "nick_name" => "Iwan",
+                "email" => "bagus.setiawan@gmail.com",
+                "password" => bcrypt("admin"),
+                "birth_date" => "1991-01-01",
+                "birth_place" => "Sukabumi",
+                "address" => "Jl. Palem indah no 14",
+                "village_id" => "3174040006",
+                "gender_id" => "1",
+                "occupation_id" => "1",
+                "education_id" => "1",
+                "marital_status_id" => "1",
+                "religion_id" => "1",
+                "avatar_id" => "1",
+                "profile" => "1",
+                "sinch_id" => "1",
+                "facebook_id" => "1"
+            ]
+        ]);
     }
 }
